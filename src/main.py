21595229e76b2f764491c4f2eaa224c08ff383bf
Francesco Pinzauti <francescopinzauti@gmail.com>
2022-05-07 17:49:18 +0200

    fix: argparse

--- src/main.py
+++ src/main.py
@@ -2,44 +2,50 @@
 entrypoint
 """
 import argparse
-import re
 import clingo
+import re
 
 
 def on_model(m):
-     
-     goal_point = re.match(r'goal\(\d,\d\)', str(m))
-     links = re.findall(r'link\(start\(\d,\d\),end\(\d,\d\)\)', str(m))
+    """
+    Prints the APS result in a prettier and more comprehensible way.
+    :param m: the model.
+    """
+    goal_point = re.match(r'goal\(\d,\d\)', str(m))
+    links = re.findall(r'link\(start\(\d,\d\),end\(\d,\d\)\)', str(m))
 
-     if goal_point:
-         print("The intersecion point is: ") 
-         print(goal_point.group() + "\n")
+    if goal_point:
+        print("The intersecion point is: ") 
+        print(goal_point.group() + "\n")
      
-     if links:
-        print("The links to get to the intersecion points are the following: \n")
+    if links:
+        print("The links to get to the intersection points are the following: \n")
         for link in links:
             print(link + '\n')
 
 
-def clingo_manual():
+def asp_manual():
     """
-
+    Manual input for ASP. You can modify the input file located in asp/data/input.lp.
     """
     ctl = clingo.Control()
-    ctl.add("base", [], """ #include "asp/data/input.lp". #include "asp/main.lp". """)
+    ctl.add("base", [], """\
+        #include "asp/data/input.lp". 
+        #include "asp/main.lp". 
+        """)
     ctl.ground([("base", [])])
     ctl.solve(on_model=on_model)
 
 def main(tool, mode):
     """
-    Main function.
-    :param tool: 
-    :param mode: 
+    Entrypoint of the program.
+    :param tool: the tool to use, either ASP or MiniZinc.
+    :param mode: if the input is manual or random.
     """
 
     if tool == "asp":
         if mode == "manual":
-            clingo_manual()
+            asp_manual()
 
 
 def init():
@@ -47,16 +53,16 @@ def init():
     It collects the arguments to pass to the main function, which is then called.
     :return: None.
     """
-    parser = argparse.ArgumentParser(description="Start the project.")
-    parser.add_argument("--tool",
+    parser = argparse.ArgumentParser(description="In order to start the project choose between ASP and MiniZinc and between manual or random mode.")
+    parser.add_argument('tool',
                         choices=["asp", "minizinc"],
                         help="Choose between ASP and MiniZinc.")
-    parser.add_argument("--mode",
+    parser.add_argument('mode',
                         choices=["manual", "random"],
                         help="Choose if the input is manual or random.")
     main(
         tool=parser.parse_args().tool,
-        mode=parser.parse_args().mode
+        mode=parser.parse_args().mode,
         )
 
 init()
