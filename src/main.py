4ba99745d49568638c14808094d1bfcc5b5a1aa8
Francesco Pinzauti <francescopinzauti@gmail.com>
2022-05-07 14:49:55 +0200

    feat: asp entrypoint

--- src/main.py
+++ src/main.py
@@ -2,9 +2,32 @@
 entrypoint
 """
 import argparse
+import random
+from clingo.control import Control
+from clingo.symbol import Function
+from clingo.symbol import Number
 
-def main():
-    pass
+def main(tool):
+
+    dimension = 3
+
+    def casual(turn = False):
+        if turn:
+            return Number(random.randint(0, dimension - 2))
+        return Number(random.randint(1, dimension))
+
+    def on_model(m):
+     print (m)
+
+    ctl = Control()
+    ctl.add("base", ["n"], 'boardl(n).')
+    ctl.add("base", ["a", "b", "c"], "number(a,b,c).")
+    ctl.add("base", [], """#include "asp/main.lp". """)
+
+    ctl.ground([("base", [])])
+    ctl.ground([("base", [Number(dimension)])])
+    ctl.ground([("base", [casual(), casual(), casual(True)]) for i in range(random.randint(2, dimension))])
+    print(ctl.solve(on_model=on_model))
 
 def init():
     """
@@ -15,6 +38,6 @@ def init():
     parser.add_argument("tool",
                         choices=["asp", "minizinc"],
                         help="Choose between ASP and MiniZinc.")
-    main(party=parser.parse_args().party)
+    main(tool=parser.parse_args().tool)
 
 init()
